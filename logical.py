0234454b52e0a9a7cf8f846da2273a93c0944b72
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-02-02 23:09:44 +0530

    initial commit

---
+++ logical.py
@@ -0,0 +1,12 @@
+# Examples of Logical Operator 
+a = True
+b = False
+
+print(a and b)               # False
+print('x and y is',a and b)
+ 
+print(a or b)                # True
+print('x or y is',a or b)
+ 
+print(not a)               # False
+print('not x is',not a)
