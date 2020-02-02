a7f725fa89061bce1b9a9e90a2db4c3712c31ab5
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-02-02 23:22:38 +0530

    Initial Commit

---
+++ Identity.py
@@ -0,0 +1,11 @@
+
+# Examples of Identity operators 
+x1 = 8
+y1 = 8
+x2 = 'Aman'
+y2 = 'Aman'
+x3 = [1,2,3]
+y3 = [1,2,3]
+print(x1 is not y1)   # False
+print(x2 is y2)       #  True
+print(x3 is y3)       # False
