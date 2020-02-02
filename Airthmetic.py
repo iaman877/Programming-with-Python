d0fb5dc63959b270f2124f4be57c8b4bcaa26d10
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-02-02 22:44:51 +0530

    Initial commit

---
+++ Airthmetic.py
@@ -0,0 +1,31 @@
+#Examples of Arithmetic Operator 
+a = 9 
+b = 4 
+add = a + b 
+print (add) 
+print ('x + y =', a + b) 
+
+sub = a - b 
+print (sub) 
+print ('x - y =', a - b) 
+ 
+mul = a * b 
+print (mul) 
+print ('x * y =', a * b) 
+ 
+div1 = a / b
+#Division(float) of number
+print (div1) 
+print ('x / y =', a / b) 
+ 
+div2 = a// b    # Division(floor) of number 
+print ('x // y =', a//b)
+print (div2) 
+ 
+mod = a % b #Modulo of both number 
+print(mod) 
+
+ 
+ 
+ 
+ 
