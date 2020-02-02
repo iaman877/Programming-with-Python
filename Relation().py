011128bba84bc0c96d4881584f00c757861568aa
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-02-02 22:58:10 +0530

    Initial commit

---
+++ Relation().py
@@ -0,0 +1,18 @@
+# Examples of Relational Operators 
+a = 13
+b = 33
+
+
+print(a > b)         #  False 
+print('x > y  is',a>b)
+print(a < b)         # True 
+print('x < y  is',a<b)
+print(a == b)        #  False 
+print('x == y is',a==b)
+print(a != b)        #  True
+print('x != y is',a!=b)
+print(a >= b)        #  False 
+print('x >= y is',a>=b)
+print(a <= b)        #  True 
+print('x <= y is',a<=b)
+
