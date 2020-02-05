1b4dd3895c51842f4fc8897dcc4089036a7d6474
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-02-05 22:20:27 +0530

    Initial commit

---
+++ Function/Slicing.py
@@ -0,0 +1,7 @@
+x = 'Hello world'
+print(x[::2])         # Hlowrd
+print(x[::3])         # Hlwl
+print(x[3:8:2])       # l o 
+print(x[-3:-8:1])
+print(x[-3:-8:-1])    # row o
+print(x[::-1])        # dlrow olleh
