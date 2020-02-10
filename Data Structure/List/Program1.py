6ec4ca8f437a414cacec0d4bae639cce52da2ede
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-02-10 22:25:51 +0530

    Creating, adding, accessing removing of elements

--- Data Structure/List/Program1.py
+++ Data Structure/List/Program1.py
@@ -30,3 +30,75 @@ print("\nList with the use of Numbers: ",List)
 List = [89, 2, 'Aman', 4.567, 'Branch is', .12, 'IOT'] 
 print("\nList with the use of Mixed Values: ",List)
 
+#---------------------------------------------------------------------------------
+# Addition of Elements in the List 
+List.append(1) 
+print("\nList after Addition of Three elements: ",List) 
+
+# Adding elements to the List using Iterator 
+for i in range(1, 4): 
+    List.append(i) 
+print("\nList after Addition of elements from 1-3: ",List)
+
+# Adding Tuples to the List 
+List.append((5, 6)) 
+print("\nList after Addition of a Tuple: ",List) 
+
+# Addition of List to a List 
+List2 = ['Aman', 'Bhardwaj'] 
+List.append(List2) 
+print("\nList after Addition of a List: ",List)
+
+#---------------------------------------------------------------------------------
+# Python program to demonstrate Addition of elements in a List ( Creating a List)
+# Important
+
+List = [1,2,3,4] 
+print("Initial List: ",List) 
+
+# Addition of Element at specific Position (using Insert Method) 
+
+List.insert(0, 'AmanDeep') 
+List.insert(3, 12) 
+print("\nList after Insert Operation: ",List) 
+
+#----------------------------------------------------------------------------
+
+# Python program to demonstrate Addition of elements in a List  Creating a List 
+List = [1,2,3,4] 
+print("Initial List: ",List)
+
+# Addition of multiple elements to the List at the end (using Extend Method) 
+
+List.extend([8, 'Be happy', 'Always']) 
+print("\nList after Extend Operation: ",List) 
+
+# Creating a Multi-Dimensional List  (By Nesting a list inside a List) 
+List = [['Happy', 'For'] , ['Aman','harish']] 
+print(List)
+print("Acessing a element from a Multi-Dimensional list") 
+print(List[0][0],List[0][1],List[1][0],List[1][1]) 
+
+#------------------------------------------------------------------------
+# Python program to demonstrate Removal of elements in a List 
+
+List = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] 
+
+List.remove(5) 
+List.remove(6) 
+print("\nList after Removal : ",List) 
+
+
+# Removing elements from List using iterator method 
+for i in range(1, 5): 
+	List.remove(i) 
+print("\n updated list : ",List) 
+#----------------------------------------------------------------------------
+List = [1,2,3,4,5] 
+
+# Removing element from the pop() method 
+List.pop() 
+print("\nList after popping an element: ",List) 
+
+List.pop(2)         # here 2 is index 
+print("\nList after popping a specific element: ",List) 
