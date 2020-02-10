b461cfd14b7c9543b935f53735ba65edc93ca8d7
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-02-10 21:57:45 +0530

    Initial Commit

--- Data Structure/List/Program1.py
+++ Data Structure/List/Program1.py
@@ -30,33 +30,3 @@ print("\nList with the use of Numbers: ",List)
 List = [89, 2, 'Aman', 4.567, 'Branch is', .12, 'IOT'] 
 print("\nList with the use of Mixed Values: ",List)
 
-# Addition of Elements in the List 
-List.append(1) 
-print("\nList after Addition of Three elements: ",List) 
-
-# Adding elements to the List using Iterator 
-for i in range(1, 4): 
-    List.append(i) 
-print("\nList after Addition of elements from 1-3: ",List)
-
-# Adding Tuples to the List 
-List.append((5, 6)) 
-print("\nList after Addition of a Tuple: ",List) 
-
-# Addition of List to a List 
-List2 = ['Aman', 'Bhardwaj'] 
-List.append(List2) 
-print("\nList after Addition of a List: ",List)
-
-# Python program to demonstrate Addition of elements in a List ( Creating a List)
-# Important
-
-List = [1,2,3,4] 
-print("Initial List: "List) 
-
-# Addition of Element at specific Position (using Insert Method) 
-
-List.insert(0, 'Geeks') 
-List.insert(3, 12) 
-print("\nList after performing Insert Operation: ",List) 
-
