46d597249567516d729b16e93c0851d9b3745563
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-02-16 22:19:47 +0530

    Basic of try , except , else & finally

--- Exceptional handling/Program1.py
+++ Exceptional handling/Program1.py
@@ -31,4 +31,12 @@ if x < 0:
  y = "hello"
 if not type(y) is int:
   raise TypeError("Only integers are allowed ")
+ #----------------------------------------------------------------------------------------------------------------
+# Program to depict Raising Exception 
+try: 
+	raise NameError("Hi there")       # Raise Error 
+except NameError: 
+	print("An exception")
+	raise # To determine whether the exception was raised or not 
+
  
