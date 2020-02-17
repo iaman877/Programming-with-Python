02d1958cdaebf29460bfea30153a13d1b801e20e
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-02-17 23:41:06 +0530

    updated

--- Exceptional handling/Program1.py
+++ Exceptional handling/Program1.py
@@ -38,5 +38,13 @@ try:
 except NameError: 
 	print("An exception")
 	raise # To determine whether the exception was raised or not 
+#-----------------------------------------------------------------------------------------------------------------
+try: 
+	k = 5//0 
+	print(k) 
+except ZeroDivisionError: 
+	print("Can't divide by zero") 
+	
+finally: 
+	print('This is always executed') 
 
- 
