8200a8afe67fe14f0851f5ecc8f8ad1cb6737e8e
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-03-24 23:04:14 +0530

    Introduction

---
+++ Constructor/code1.py
@@ -0,0 +1,7 @@
+class Main: 
+	def __init__(self):  	# default constructor
+		self.g= "Aman"
+	def p(self):    # a method for printing data members
+		print(self.g) 
+obj = Main()       # creating object of the class 
+obj.p()           # calling the instance method using the object obj 
