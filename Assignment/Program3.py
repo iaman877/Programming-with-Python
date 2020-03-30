7341c64e7f30d922eb778dee546a4ebb5dc09e3c
Aman Bhardwaj <49730521+BhardwajAman199@users.noreply.github.com>
2020-03-30 13:43:06 +0530

    Create Program3.py

---
+++ Assignment/Program3.py
@@ -0,0 +1,12 @@
+# . Write a program to print the sum of the following series 1 + 1/2 + 1/3 +. …. + 1/n
+
+def sum(n): 
+	i = 1
+	s = 0.0
+	for i in range(1, n+1): 
+		s = s + 1/i; 
+	return s; 
+
+# Driver Code 
+n = 5
+print("Sum is", round(sum(n), 6)) 
