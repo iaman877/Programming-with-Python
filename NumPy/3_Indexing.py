a69de7296686164473a09fc06643d4359089c2bf
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2020-10-26 18:18:35 +0530

    negative indexing updated

--- NumPy/3_Indexing.py
+++ NumPy/3_Indexing.py
@@ -12,3 +12,4 @@ print('2nd element on 1st dim: ', arr[0, 1])  #output 2nd element on 1st dim:  2
 import numpy as np
 arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
 print('Last element from 2nd dim: ', arr[1, -1]) #output Last element from 2nd dim:  10
+
