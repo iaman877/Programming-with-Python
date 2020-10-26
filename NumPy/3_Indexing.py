72d1015c30b19f5007b0d04f24a456401bda1b0f
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2020-10-26 17:44:03 +0530

    till 2d array updated

--- NumPy/3_Indexing.py
+++ NumPy/3_Indexing.py
@@ -1,1 +1,10 @@
+# Access 1-D Arrays
+import numpy as np
+arr = np.array([1, 2, 3, 4])
+print(arr[0])   #output 1
+
+# Access 2-D Arrays
+import numpy as np
+arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+print('2nd element on 1st dim: ', arr[0, 1])  #output 2nd element on 1st dim:  2
 
